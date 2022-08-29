7edfb37962f881ee166e4e23ec9defad6ad7a9b1
iskanderbelhouchet <78920370+iskanderbelhouchet@users.noreply.github.com>
2022-08-29 16:02:22 +0100

    1

--- uploader/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from uploader.models import ImageFile
-
-
-admin.site.register(ImageFile)
--- uploader/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from uploader.models import ImageFile
-
-
-class ImageFileForm(forms.ModelForm):
-    class Meta:
-        model = ImageFile
-        fields = ('image', )
